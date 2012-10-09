ec749e8583aeaf176fe353d049f3d08584926d33
Alex Wolfe <alexkimiwolfe@gmail.com>
2012-10-08 22:00:18 -0400

    added physics class

---
+++ src/alex/physicsMode.h
@@ -0,0 +1,16 @@
+/*-----------------------------------------------------------*
+ Physics Mode for AiVDJ
+
+ created by: Alex Wolfe
+ *-----------------------------------------------------------*/
+
+#include "ofMain.h"
+
+class physicsMode{
+	public:
+		physicsMode();
+		~physicsMode();
+
+		void update();
+		void render();
+}
